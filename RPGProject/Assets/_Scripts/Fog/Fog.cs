079d6bcf2612671488ca3480873d5a9fb64615a1
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-10-11 00:26:34 +0700

    Visuals - Improve art visual.

---
+++ RPGProject/Assets/_Scripts/Fog/Fog.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerMap>() != null)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
