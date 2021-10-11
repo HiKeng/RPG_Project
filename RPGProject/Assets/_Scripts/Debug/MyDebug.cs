9a61c5cc7c55ef6a71912d99bdbb2ba96f4b779e
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-10-11 17:54:10 +0700

    Minor adjustment.

--- RPGProject/Assets/_Scripts/Debug/MyDebug.cs
+++ RPGProject/Assets/_Scripts/Debug/MyDebug.cs
@@ -5,16 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MyDebug : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform _player;
+    [SerializeField] Transform _spawnPoint;
+    [SerializeField] Vector3 _threshold;
+
+    [SerializeField] GameObject _fogGroup;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
         {
              SceneManager.LoadScene(Application.loadedLevel);
+        }
+
+        if(_player.position.y <= _threshold.y)
+        {
+            _player.position = _spawnPoint.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if(_fogGroup.active)
+            {
+                _fogGroup.SetActive(false);
+            }
+            else
+            {
+                _fogGroup.SetActive(true);
+            }
         }
     }
 }
