bb8d8c4ad3a9b1ff3a04073b0f0cf9deabec2f31
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-10-25 16:42:46 +0700

    Creatures - Added spawn reward on dead.

--- RPGProject/Assets/_Scripts/Collectible/Collectibles.cs
+++ RPGProject/Assets/_Scripts/Collectible/Collectibles.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(AudioSource))]
 public class Collectibles : MonoBehaviour
 {
     [SerializeField] int _staminaRecoverAmount = 1;
--- RPGProject/Assets/_Scripts/Enemy/EnemyHealth.cs
+++ RPGProject/Assets/_Scripts/Enemy/EnemyHealth.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 
 public class EnemyHealth : UnitHealth
 {
+    [SerializeField] List<GameObject> _rewardList = new List<GameObject>();
+    public void _SpawnReward()
+    {
+        for (int i = 0; i < _rewardList.Count; i++)
+        {
+            Instantiate(_rewardList[i], transform.position, Quaternion.identity);
+        }
+    }
 }
