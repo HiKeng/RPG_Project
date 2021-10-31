0645f9d0558a5db1892624b4b84002652e48d9b0
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-10-31 20:51:53 +0700

    Level - WIP sample level.

--- RPGProject/Assets/_Scripts/Manager/DayNight_Manager.cs
+++ RPGProject/Assets/_Scripts/Manager/DayNight_Manager.cs
@@ -5,31 +5,4 @@ using UnityEngine;
 public class DayNight_Manager : SingletonBase<DayNight_Manager>
 {
     public bool _isDay = true;
-
-    //UnitLight[] _unitLights;
-
-    //private void Awake()
-    //{
-    //    _unitLights = GameObject.FindObjectsOfType<UnitLight>();
-    //}
-
-    //private void Update()
-    //{
-    //    if(_isDay)
-    //    {
-    //        _setActiveLight(false);
-    //    }
-    //    else
-    //    {
-    //        _setActiveLight(true);
-    //    }
-    //}
-
-    //void _setActiveLight(bool _isActive)
-    //{
-    //    for (int i = 0; i < _unitLights.Length; i++)
-    //    {
-    //        _unitLights[i].gameObject.SetActive(_isActive);
-    //    }
-    //}
 }
