5503d813601381bf1333fd57e731f89301a7f41d
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-11-07 13:37:43 +0700

    Player level - Added receive skill point on level up.

--- RPGProject/Assets/_Scripts/Player/PlayerLevel.cs
+++ RPGProject/Assets/_Scripts/Player/PlayerLevel.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerLevel : MonoBehaviour
 {
+    [Header("Level")]
     [SerializeField] int _currentLevel = 1;
 
     [SerializeField] float _experienceBar = 50f;
     [SerializeField] float _currentExperienceProgress;
+
+    [Header("Skill Point")]
+    [SerializeField] int _skillPointPerLevel = 5;
+    [SerializeField] int _currentSkillPoint;
+
+
+    [Header("Events")]
+    [SerializeField] UnityEvent _onGainExperience;
+    [SerializeField] UnityEvent _onLevelUp;
 
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
-            _GetLevelProgress(100f);
+            _ReceiveLevelProgress(100f);
         }
 
         _checkLevelUpProgress();
@@ -35,10 +46,20 @@ public class PlayerLevel : MonoBehaviour
     void _levelUp(int _levelUpAmount)
     {
         _currentLevel += _levelUpAmount;
+        _ReceiveSkillPoint();
+
+        _onLevelUp.Invoke();
     }
 
-    public void _GetLevelProgress(float _amountToReceive)
+    public void _ReceiveLevelProgress(float _amountToReceive)
     {
         _currentExperienceProgress += _amountToReceive;
+
+        _onGainExperience.Invoke();
+    }
+
+    public void _ReceiveSkillPoint()
+    {
+        _currentSkillPoint += _skillPointPerLevel;
     }
 }
