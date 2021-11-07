678d9e026287ab886aec19ba56b3e08d01aa7701
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-11-07 13:31:09 +0700

    Player level - Added level up functions.

--- RPGProject/Assets/_Scripts/Player/PlayerLevel.cs
+++ RPGProject/Assets/_Scripts/Player/PlayerLevel.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class PlayerLevel : MonoBehaviour
 {
+    [SerializeField] int _currentLevel = 1;
 
+    [SerializeField] float _experienceBar = 50f;
+    [SerializeField] float _currentExperienceProgress;
 
-    [SerializeField] float _levelUpBar = 50f;
-    [SerializeField] float _currentLevelUpProgress;
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            _GetLevelProgress(100f);
+        }
+
+        _checkLevelUpProgress();
+    }
+
+    void _checkLevelUpProgress()
+    {
+        if(_currentExperienceProgress >= _experienceBar)
+        {
+            while (_currentExperienceProgress >= _experienceBar)
+            {
+                _currentExperienceProgress -= _experienceBar;
+                _levelUp(1);
+            }
+        }
+    }
+
+    void _levelUp(int _levelUpAmount)
+    {
+        _currentLevel += _levelUpAmount;
+    }
+
+    public void _GetLevelProgress(float _amountToReceive)
+    {
+        _currentExperienceProgress += _amountToReceive;
     }
 }
