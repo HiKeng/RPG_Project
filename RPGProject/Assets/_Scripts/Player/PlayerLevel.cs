722a2e52f851f723a41d5088e6ab12e855fd5530
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-11-07 13:07:15 +0700

    Player level - Added script file.

---
+++ RPGProject/Assets/_Scripts/Player/PlayerLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+
+
+    [SerializeField] float _levelUpBar = 50f;
+    [SerializeField] float _currentLevelUpProgress;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
