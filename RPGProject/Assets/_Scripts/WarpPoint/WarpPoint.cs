625cd6e1b01907f2b994c38c8e655e824bbd67d8
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-11-20 17:20:15 +0700

    Level - WIP Adding level sections.

--- RPGProject/Assets/_Scripts/WarpPoint/WarpPoint.cs
+++ RPGProject/Assets/_Scripts/WarpPoint/WarpPoint.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(SphereCollider))]
 public class WarpPoint : MonoBehaviour
 {
-    [SerializeField] string _stageToGo;
+    [Header("Reference")]
+    [SerializeField] WarpPoint _positionToGo;
+    [SerializeField] Transform _SpawnPoint;
+
+    [Header("Events")]
+    [SerializeField] UnityEvent _onWarp;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == Unit_Manager.Instance._player)
         {
-            SceneManager.LoadScene(_stageToGo);
+            _warpToPosition();
         }
+    }
+
+    void _warpToPosition()
+    {
+        Unit_Manager.Instance._player.transform.position = _positionToGo._SpawnPoint.position;
     }
 }
